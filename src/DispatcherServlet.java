de971b069aee5e40b3830545b05583ca0995b855
Pengyu Wang <peng.wang@arestravelinc.com>
2015-03-25 17:03:19 -0700

    add db helper

---
+++ src/DispatcherServlet.java
@@ -0,0 +1,25 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/DispatcherServlet")
+public class DispatcherServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter output = response.getWriter();
+        output.println("hello3");
+        output.close();
+
+        // TODO: fetch all inventory
+
+        HttpSession session = request.getSession(true);
+    }
+}
