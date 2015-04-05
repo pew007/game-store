439b602ce55a642193312a1f77cb858bcc449c07
Pengyu Wang <bird0407@gmail.com>
2015-04-05 10:43:08 -0700

    angular test run

--- src/GetProductList.java
+++ src/GetProductList.java
@@ -7,7 +7,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @WebServlet("/product/list")
 public class GetProductList extends HttpServlet {
@@ -18,16 +20,16 @@ public class GetProductList extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         ArrayList<Product> products = Product.getProducts();
-        ArrayList<Vendor> vendors = Vendor.getVendors();
-        ArrayList<Category> categories = Category.getCategories();
-        ArrayList<Platform> platforms = Platform.getPlatforms();
+        ArrayList<String> jsonResponse = new ArrayList<String>();
 
-        HttpSession session = request.getSession(true);
-        session.setAttribute("products", products);
-        session.setAttribute("vendors", vendors);
-        session.setAttribute("categories", categories);
-        session.setAttribute("platforms", platforms);
+        for (Product product : products) {
+            String json = product.toJsonString();
+            jsonResponse.add(json);
+        }
 
-        request.getRequestDispatcher("/WEB-INF/jsp/productList.jsp").forward(request, response);
+        response.setContentType("application/json");
+        PrintWriter output = response.getWriter();
+        output.println(jsonResponse);
+        output.close();
     }
 }
--- web/js/app.js
+++ web/js/app.js
@@ -1,60 +1,12 @@
-$(document).ready(function(){
+(function(){
 
-  $('button').button();
+  $('.button').button();
 
-  setupSidebarMenu();
-  setupSortEventHandling();
+  var storeApp = angular.module('storeApp', []);
 
-  function setupSidebarMenu() {
-    $( "#mainContentSidebarMenu" ).menu({
-      items: "> :not(.ui-widget-header)"
+  storeApp.controller('StoreCtrl', function($scope, $http){
+    $http.get('/product/list').success(function(data){
+      $scope.products = data;
     });
-  }
-
-  function setupSortEventHandling() {
-    $('#sortBar').find('a[data-sortby]').click(function () {
-      var sortBy = $(this).data('sortby');
-      var sortQueue = $('.productItem');
-
-      if (sortBy === 'price-low') {
-        sortQueue.sort(sortByLowPrice);
-      } else if (sortBy === 'price-high') {
-        sortQueue.sort(sortByHighPrice);
-      } else if (sortBy === 'name') {
-        sortQueue.sort(sortByProductName);
-      } else if (sortBy === 'platform') {
-        sortQueue.sort(sortByPlatform);
-      }
-
-      sortQueue.each(function (index, value) {
-        var productItem = $(value);
-        $('#productList').append(productItem);
-      });
-
-      return false;
-    });
-  }
-
-  function sortByLowPrice(a, b) {
-    return parseInt($(a).attr('data-price')) - parseInt($(b).attr('data-price'));
-  }
-
-  function sortByHighPrice(a, b) {
-    return parseInt($(b).attr('data-price')) - parseInt($(a).attr('data-price'));
-  }
-
-  function sortByProductName(a, b) {
-    if ($(b).attr('data-name') == $(a).attr('data-name')) {
-      return 0;
-    }
-    return $(b).attr('data-name') > $(a).attr('data-name') ? -1 : 1;
-  }
-
-  function sortByPlatform(a, b) {
-    if ($(b).attr('data-platform') == $(a).attr('data-platform')) {
-      return 0;
-    }
-    return $(b).attr('data-platform') > $(a).attr('data-platform') ? -1 : 1;
-  }
-
-});
+  });
+})();
